a3eb35e6842e843c4b6b5fa251e5f71deb560099
ydicsey <67023584+ydicsey@users.noreply.github.com>
2022-02-19 22:01:44 +0800

    Create 704. Binary Search.c
    32 ms / 7 MB

---
+++ leetcode/basic-algorithm/704. Binary Search.c
@@ -0,0 +1,16 @@
+int search(int* nums, int numsSize, int target){
+    int left = 0;
+    int right = numsSize-1;
+    int pivot;
+    while (left <= right)
+    {
+        pivot = (left + right) / 2;
+        if (nums[pivot] == target)
+            return pivot;
+        if (nums[pivot] < target)
+            left = pivot + 1;
+        else if (nums[pivot] > target)
+            right = pivot - 1;
+    }
+    return -1;
+}
