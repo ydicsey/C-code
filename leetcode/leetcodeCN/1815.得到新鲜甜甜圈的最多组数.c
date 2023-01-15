ed791922d76f21a332ab44e6a8e435d57b93c049
ydicsey <67023584+ydicsey@users.noreply.github.com>
2023-01-15 06:30:00 +0000

    test

---
+++ leetcode/leetcodeCN/1815.得到新鲜甜甜圈的最多组数.c
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1815 lang=c
+ *
+ * [1815] 得到新鲜甜甜圈的最多组数
+ */
+
+// @lc code=start
+
+
+int maxHappyGroups(int batchSize, int* groups, int groupsSize){
+
+}
+// @lc code=end
+
