91539ad543a3914f558182bd01c8370b20b81bbe
ydicsey <67023584+ydicsey@users.noreply.github.com>
2023-05-02 14:57:51 +0000

    AC

---
+++ virtual/UVA_10038.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    int N = 0;
+    while(cin >> N)
+    {
+        vector<int> dis;
+        vector<int> ori;
+        int pre = 0, cur = 0;
+        cin >> pre;
+        for(int i = 1; i < N; i++)
+        {
+            ori.emplace_back(i);
+            cin >> cur;
+            dis.emplace_back(abs(pre - cur));
+            pre = cur;
+        }
+        // sort it
+        sort(dis.begin(), dis.end());
+        int flag = 0;
+        for(int i = 0; i < dis.size(); i++)
+        {
+            if(dis[i] != ori[i])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag)
+        {
+            cout << "Not jolly" << endl;
+        }
+        else
+        {
+            cout << "Jolly" << endl;
+        }
+    }
+}
