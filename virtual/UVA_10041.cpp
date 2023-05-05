6a0f7f26d5698fcb8dfa5bee4b2e3c71dd00e54c
ydicsey <67023584+ydicsey@users.noreply.github.com>
2023-05-05 19:17:00 +0000

    AC

--- virtual/UVA_10041.cpp
+++ virtual/UVA_10041.cpp
@@ -1,34 +1,34 @@
-    #include <iostream>
-    #include <algorithm>
-    #include <vector>
-    #include <cmath>
-    using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
 
-    int main()
+int main()
+{
+    int N = 0;
+    cin >> N;
+    while(N--)
     {
-        int N = 0;
-        cin >> N;
-        while(N--)
-        {
-            int M = 0;
-            cin >> M;
+        int M = 0;
+        cin >> M;
 
-            vector<int> arr;
-            while(M--)
-            {
-                int tmp = 0;
-                cin >> tmp;
-                arr.emplace_back(tmp);
-            }
-            // sort(arr.begin(), arr.end());
-            auto mid = (arr.size() >> 1);
-            auto m = arr.begin() + mid;
-            nth_element(arr.begin(), m, arr.end());
-            int sum = 0;
-            for(int i = 0; i < arr.size(); ++i)
-            {
-                sum += abs(arr[i]- arr[mid]);
-            }
-            cout << sum <<endl;
+        vector<int> arr;
+        while(M--)
+        {
+            int tmp = 0;
+            cin >> tmp;
+            arr.emplace_back(tmp);
         }
+        // sort(arr.begin(), arr.end());
+        auto mid = (arr.size() >> 1);
+        auto m = arr.begin() + mid;
+        nth_element(arr.begin(), m, arr.end());
+        int sum = 0;
+        for(int i = 0; i < arr.size(); ++i)
+        {
+            sum += abs(arr[i]- arr[mid]);
+        }
+        cout << sum <<endl;
     }
+}
