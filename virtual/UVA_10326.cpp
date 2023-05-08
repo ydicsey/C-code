d0c626cee336cb1ac9abb1f9670705dfab13327b
ydicsey <67023584+ydicsey@users.noreply.github.com>
2023-05-08 14:49:40 +0000

    not finished

---
+++ virtual/UVA_10326.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    while(cin >> N)
+    {
+        int n = 0;
+        cin >> n;
+        vector<long long int> root(n+1);
+        for(int i = 0; i < n; ++i)
+        {
+            cin >> root[i];
+        }
+    }
+}
