db9d81dab7fe9e2775a9c22b6d54610545843f8e
ydicsey <67023584+ydicsey@users.noreply.github.com>
2024-05-18 06:43:45 +0000

    AC

--- leetcode/leetcodeCN/1553.吃掉-n-个橘子的最少天数.cpp
+++ leetcode/leetcodeCN/1553.吃掉-n-个橘子的最少天数.cpp
@@ -6,9 +6,23 @@
 
 // @lc code=start
 class Solution {
+    // 動態規劃
+    unordered_map<int, int>DP;
 public:
     int minDays(int n) {
-
+        // cout << "n:" << n << endl;
+        if(n <= 2)
+        {
+            return n;
+        }
+        // 連續吃掉k個使橘子為或3的倍數
+        // recursive
+        if(DP.count(n))
+        {
+            return DP[n];
+        }
+        DP[n] = 1 + min(n%3 + minDays(n/3), n%2 + minDays(n/2));
+        return DP[n];
     }
 };
 // @lc code=end
