abf4edfc8d199f042d0d630a5c2442a58ef0de1e
ydicsey <67023584+ydicsey@users.noreply.github.com>
2024-09-12 09:12:22 +0000

    from extension

---
+++ leetcode/leetcodeCN/2555.maximize-win-from-two-segments.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=2555 lang=cpp
+ * @lcpr version=30204
+ *
+ * [2555] 两个线段获得的最多奖品
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int maximizeWin(vector<int>& prizePositions, int k) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,1,2,2,3,3,5]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4]\n0\n
+// @lcpr case=end
+
+ */
+
