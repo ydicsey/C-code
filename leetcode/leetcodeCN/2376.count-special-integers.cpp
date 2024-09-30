9d062b5456b96a88f52ec22fe4f59d89ca0f36c8
ydicsey <67023584+ydicsey@users.noreply.github.com>
2024-09-30 08:33:33 +0000

    too difficult

---
+++ leetcode/leetcodeCN/2376.count-special-integers.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=2376 lang=cpp
+ * @lcpr version=30204
+ *
+ * [2376] 统计特殊整数
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int countSpecialNumbers(int n) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 20\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 135\n
+// @lcpr case=end
+
+ */
+
--- leetcode/leetcodeCN/myleetcode_solution.md
+++ leetcode/leetcodeCN/myleetcode_solution.md
@@ -12,5 +12,17 @@
 亦開始會覺得為DP, 但看完題解不是, 而是 divide and conquer, 把運算子左右兩邊切開, 並遞迴下去, 再進行運算
 
   - 參考：[題解](https://labuladong.online/algo/practice-in-action/divide-and-conquer/)
-  - 補充DP的括號問題：[括號匹配](https://leetcode.com/problems/generate-parentheses/description/)
+  - 補充DP的括號問題：[括號匹配](https://leetcode.com/problems/generate-parentheses/description/)、
   矩陣相乘...
+
+## leetcode 2376
+
+找出特殊數字, 數字範圍超大
+
+- 數位DP
+
+> 利用位運算(bitmask)把 int 當成set來用
+>> {0, 2, 3} => bin (1, 1, 0, 1) = dex(13)
+>>> mask 是為了判斷東西有沒有重複
+
+$ leetcode 1012, 357 
