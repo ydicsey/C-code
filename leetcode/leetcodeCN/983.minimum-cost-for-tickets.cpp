af7eaac219a24c4edac25702d06333cd038154eb
ydicsey <67023584+ydicsey@users.noreply.github.com>
2024-11-02 06:36:53 +0000

    not finished yet

---
+++ leetcode/leetcodeCN/983.minimum-cost-for-tickets.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=983 lang=cpp
+ * @lcpr version=30204
+ *
+ * [983] 最低票价
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        unordered_map<int, int> dp;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,4,6,7,8,20]\n[2,7,15]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5,6,7,8,9,10,30,31]\n[2,7,15]\n
+// @lcpr case=end
+
+ */
+
